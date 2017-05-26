7ea2d3705bc834e7d3ced8cff03ae1092bdec0cd
Seokin4862 <sy34@rice.edu>
2017-05-26 09:51:47 -0500

    phicheck

--- modules/PicoDstSkimmer/PicoDstSkimmer.h
+++ modules/PicoDstSkimmer/PicoDstSkimmer.h
@@ -50,22 +50,47 @@ protected:
 	TH1D *hDedx = 0;
 	TH1D *hphi = 0;
 	TH1D *hRunN = 0;
+	TH1D *hphicheck = 0;
 
-	TH2D *hMeanRunN1 = 0;
-	TH2D *hMeanRunN2 = 0;
+	TH2D *hMeanRunN01 = 0;
+	TH2D *hMeanRunN02 = 0;
+	TH2D *hMeanRunN03 = 0;
+
+	TH2D *hMeanRunN04 = 0;
+	TH2D *hMeanRunN05 = 0;
+	TH2D *hMeanRunN06 = 0;
+
+	TH2D *hMeanRunN07 = 0;
+	TH2D *hMeanRunN08 = 0;
+	TH2D *hMeanRunN09 = 0;
+
+
+	TH2D *hMeanRunN11 = 0;
+	TH2D *hMeanRunN12 = 0;
+	TH2D *hMeanRunN13 = 0;
+
+	TH2D *hMeanRunN14 = 0;
+	TH2D *hMeanRunN15 = 0;
+	TH2D *hMeanRunN16 = 0;
+
+	TH2D *hMeanRunN17 = 0;
+	TH2D *hMeanRunN18 = 0;
+	TH2D *hMeanRunN19 = 0;
 
 	void makeHistograms(){
 
 		hDedx = new TH1D( "dEdx", "dEdx of Various Tracks; x-axis; y-axis", 1000, 0, 30 );
 		hphi = new TH1D( "phi", "phi of Various Tracks; x-axis; y-axis", 1000, -3.2, 3.2 );
 		hRunN = new TH1D( "RunN", "Run Numbers", 2751, 1, 2751 );
+		hphicheck = new TH1D( "phi check", "phi of Various Tracks; x-axis, y-axis", 1000, -3.2, 3.2 );
+
+		//add stuff here. like, a lot of stuff
 
 		hMeanRunN1 = new TH2D( "hMeanRunN1" , " dEdx vs run number for Momentum between .3 and .4, phi between 2.56 and 2.60 ; run number ; dE/dx ", 2751, 1, 2751, 1000, 0, 6 );
 		hMeanRunN2 = new TH2D( "hMeanRunN2" , " dEdx vs run number for Momentum between .3 and .4, phi between 2.56 and 2.60 ; run number ; dE/dx ", 2751, 1, 2751, 1000, 0, 20 );
 
 
 	}
-
 
 	virtual void analyzeEvent() {
 		StPicoEvent *event = _rEvent.get( 0 );
@@ -80,15 +105,23 @@ protected:
 
 			if( track->pMom().mag() < .3 ) continue;
 			if( track->pMom().mag() > .4 ) continue;
-			if( track->pMom().phi() < 2.56 ) continue;
-			if( track->pMom().phi() > 2.60 ) continue;
+//			if( track->pMom().phi() < 2.56 ) continue;
+//			if( track->pMom().phi() > 2.60 ) continue;
 
 			hDedx->Fill( track->dEdx() );
 			hphi->Fill( track->pMom().phi() );
 			hRunN->Fill( rmf.indexForRun( event->runId() ) );
 
-			hMeanRunN1->Fill( rmf.indexForRun( event->runId() ) , track->dEdx() );
-			hMeanRunN2->Fill( rmf.indexForRun( event->runId() ) , track->dEdx() );
+			if( track->pMom().phi() > 2.56 && track->pMom().phi() < 2.60 ){
+
+				hphicheck->Fill( track->pMom().phi() );
+
+
+
+			}
+
+	//		hMeanRunN1->Fill( rmf.indexForRun( event->runId() ) , track->dEdx() );
+	//		hMeanRunN2->Fill( rmf.indexForRun( event->runId() ) , track->dEdx() );
 
 
 
