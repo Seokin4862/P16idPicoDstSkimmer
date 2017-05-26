1b1f38281c79eb5b869b527d17d7137e9030fb8a
Seokin4862 <sy34@rice.edu>
2017-05-26 08:41:30 -0500

    nSigmaElectron2

--- modules/PicoDstSkimmer/PicoDstSkimmer.h
+++ modules/PicoDstSkimmer/PicoDstSkimmer.h
@@ -91,8 +91,8 @@ protected:
 
 			if( track->pMom().mag() < .3 ) continue;
 			if( track->pMom().mag() > .4 ) continue;
-			if( track->nSigmaElectron() >  2 ) continue;
-			if( track->nSigmaElectron() < -2 ) continue;
+			if( track->nSigmaElectron() >  1 ) continue;
+			if( track->nSigmaElectron() < -1 ) continue;
 
 			hDedx->Fill( track->dEdx() );
 			hphi->Fill( track->pMom().phi() );
