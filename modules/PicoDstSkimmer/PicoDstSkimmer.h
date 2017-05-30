bfd8aafd1b3dcfec4cf65c533366cf2e4d5d3c91
Seokin4862 <sy34@rice.edu>
2017-05-30 16:22:39 -0500

    meg

--- modules/PicoDstSkimmer/PicoDstSkimmer.h
+++ modules/PicoDstSkimmer/PicoDstSkimmer.h
@@ -53,7 +53,7 @@ protected:
 		hdEdx = new TH1D( "dEdx", "dE/dx of Various Tracks; dE/dx; counts", 1000, 0, 30 );
 		hphi = new TH1D( "phi", "phi of Various Tracks; x-axis; y-axis", 1000, -3.2, 3.2 );
 
-		hdEdxvsMom = new TH2D( "dE/dx vs Total Momentum of Various Tracks; Total Momentum; dE/dx", 1000, -100, 100, 1000, 0, 100 )
+		hdEdxvsMom = new TH2D( "dEdxvsMom", "dE/dx vs Total Momentum of Various Tracks; Total Momentum; dE/dx", 1000, -100, 100, 1000, 0, 100 )
 	};
 
 
