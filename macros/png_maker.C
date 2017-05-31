380b263ccfc3ca5168c318b957238a307285f8c9
Seokin4862 <sy34@rice.edu>
2017-05-31 16:02:47 -0500

    png_maker.C

--- macros/png_maker.C
+++ macros/png_maker.C
@@ -14,7 +14,16 @@ void png_maker( ) {
       TH1 * h = (TH1*)f->Get( sstr.str().c_str() );
       if ( 0 == h ) continue;
       cout << "i am a histogram : " << h->GetName() <<endl;
+      h->Draw();
 
+      // this ould empty out the sstr
+      // sstr.str( "" );
+      string cn = h->Class_Name();
+      if ( "TH2D" == cn ){
+        cout << "I am 2D" << endl;
+      }
+      sstr << ".png";
+      c->Print( sstr.str().c_str() );
 
   }
 
