f2eaa062f528de9921d3865ea6f65c7858778c6d
Seokin4862 <sy34@rice.edu>
2017-05-31 15:52:54 -0500

    png_maker.C

---
+++ macros/png_maker.C
@@ -0,0 +1,19 @@
+
+
+
+void png_maker( ) {
+
+  // TFile * f = new TFile( "/home/sy34/workspace/safe/dEdxvsphi/Pion1Data/nSigmaPion1.root" );
+
+  string name = "dEdxphi";
+  for ( int i = 0; i < 10; i++ ){
+      stringstream sstr;
+      sstr << name << i;
+      cout << "name of histo: " << sstr << endl;
+  }
+
+
+
+
+
+}
