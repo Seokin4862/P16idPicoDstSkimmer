d40faffec8ab27efc85aeb0b7c962cee3d41add7
Seokin4862 <sy34@rice.edu>
2017-05-31 15:57:39 -0500

    png_maker.C

--- macros/png_maker.C
+++ macros/png_maker.C
@@ -3,13 +3,19 @@
 
 void png_maker( ) {
 
-  // TFile * f = new TFile( "/home/sy34/workspace/safe/dEdxvsphi/Pion1Data/nSigmaPion1.root" );
+  TFile * f = new TFile( "/home/sy34/workspace/safe/dEdxvsphi/Pion1Data/nSigmaPion1.root" );
 
   string name = "dEdxphi";
   for ( int i = 0; i < 10; i++ ){
       stringstream sstr;
       sstr << name << i;
       cout << "name of histo: " << sstr.str() << endl;
+
+      TH1 * h = (TH11*)f->Get( sstr.str().c_str() );
+
+      cout << "i am a histogram : " << h->GetName() <<endl;
+
+
   }
 
 
