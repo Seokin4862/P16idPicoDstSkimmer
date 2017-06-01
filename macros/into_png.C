54265800f7764002d9a46bd538a865b6a1bb5a45
Seokin4862 <sy34@rice.edu>
2017-06-01 10:48:08 -0500

    -m

--- macros/into_png.C
+++ macros/into_png.C
@@ -4,7 +4,7 @@ void into_png() {
 
 TFile * f = new TFile( "/home/sy34/workspace/safe/dEdxvsphi/Pion1Data/nSigmaPion1.root" );
 
-for( int i = 1 ; i = 8 ; i++  ) {
+for( int i = 1 ; i < 8 ; i++  ) {
 stringstream refer;
 refer << "histo_name" << i;
 cout<< "name of histogram: " << refer.str() << endl;
