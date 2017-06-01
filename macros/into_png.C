7f37b23f15d6c01646624c861411130c9d38cfab
Seokin4862 <sy34@rice.edu>
2017-06-01 11:56:21 -0500

    something

--- macros/into_png.C
+++ macros/into_png.C
@@ -4,10 +4,20 @@ void into_png() {
 
 TFile * f = new TFile( "/home/sy34/workspace/safe/dEdxvsphi/Pion1Data/nSigmaPion1.root" );
 
-for( int i = 1 ; i < 8 ; i++  ) {
+TCanvas * c = new TCanvas("c","c");
+
+for( int i = 1 ; i < 20 ; i++  ) {
+  // i < number - 1
+if(i % 10 == 0) continue;
 stringstream refer;
-refer << "histo_name" << i;
-cout<< "name of histogram: " << refer.str() << endl;
+refer << "dEdxphi" << i;
+
+TH2 *h = (TH2*)f->Get( refer.str().c_str());
+if( 0 == h ) continue;
+h->Draw("colz");
+
+c1->print( refer.str() )
+
 }
 
 }
