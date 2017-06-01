776bb7190ba992ea23499789e44c7eaec203d2f7
Seokin4862 <sy34@rice.edu>
2017-06-01 13:36:45 -0500

    -m

--- macros/into_png.C
+++ macros/into_png.C
@@ -24,9 +24,17 @@ TProfile * hpx = h->ProfileX();
 hpx->Draw();
 stringstream name_px;
 name_px << refer.str() << "_px" << ".png";
-c->Print ( name_px.str().c_str() );
+c->Print( name_px.str().c_str() );
 
+h->FitSlicesY();
+TH1 * h_0 = (TH1*)gDirectory->Get( (refer + "_0").c_str() );
+TH1 * h_1 = (TH1*)gDirectory->Get( (refer + "_1").c_str() );
+TH1 * h_2 = (TH1*)gDirectory->Get( (refer + "_2").c_str() );
 
+h_1->Draw();
+c->Print( (refer+"_1"+".png").c_str());
+h_2->Draw();
+c->Print( (refer+"_2"+".png").c_str());
 
 
 }
