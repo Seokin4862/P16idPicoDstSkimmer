945f10a5330f412ef1bfcaf1227889c989e48b92
Seokin4862 <sy34@rice.edu>
2017-06-07 11:18:31 -0500

    label

--- modules/PicoDstSkimmer/PicoDstSkimmer.h
+++ modules/PicoDstSkimmer/PicoDstSkimmer.h
@@ -65,18 +65,18 @@ protected:
 
 	void makeHistograms(){
 
-		hDedx = new TH1D( "dEdx", "dEdx of Various Tracks; x-axis; y-axis", 1000, 0, 30 );
-		hphi = new TH1D( "phi", "phi of Various Tracks; x-axis; y-axis", 1000, -3.2, 3.2 );
-		hRunN = new TH1D( "RunN", "Run Numbers", 2751, 1, 2751 );
+		// hDedx = new TH1D( "dEdx", "dEdx of Various Tracks; x-axis; y-axis", 1000, 0, 30 );
+		// hphi = new TH1D( "phi", "phi of Various Tracks; x-axis; y-axis", 1000, -3.2, 3.2 );
+		// hRunN = new TH1D( "RunN", "Run Numbers", 2751, 1, 2751 );
 
-		hMatchFlag = new TH1D( "MatchFlag", "MatchFlag of Various Tracks; x-axis; y-axis", 3, -.5, 2.5);
-		hBeta = new TH1D( "Beta", "Beta of Various Tracks; x-axis; y-axis", 1000, 0, 1);
-		hInvBeta = new TH1D( "InvBeta", "1/Beta of Various Tracks; x-axis; y-axis", 1000, 0, 1.5);
-		hYLocal = new TH1D( "YLocal", "YLocal of Various Tracks; x-axis; y-axis", 100, -5, 5);
-		hZLocal = new TH1D( "ZLocal", "XLocal of Various Tracks; x-axis; y-axis", 100, -5, 5);
-		hChargePt = new TH1D( "ChargePt", " Charge times Pt of Various tracks; x-axis; y-axis", 100, -10, 10 );
-		hPMom = new TH1D( "PMom", "PMom of Various tracks; x-axis;y-axis", 100, -10, 10 );
-		hChargePMom = new TH1D( "ChargePMom", "Charge times Momentum of Various tracks; x-axis; y-axis", 100, -10, 10 );
+		hMatchFlag = new TH1D( "MatchFlag", "MatchFlag of Various Tracks; Match Flag Numer (0, 1, 2); number of tracks", 3, -.5, 2.5);
+		hBeta = new TH1D( "Beta", "Beta of Various Tracks; Beta, number of tracks", 1000, 0, 1);
+		hInvBeta = new TH1D( "InvBeta", "1/Beta of Various Tracks; 1/Beta; number of tracks", 1000, 0, 1.5);
+		hYLocal = new TH1D( "YLocal", "YLocal of Various Tracks; YLocal; number of tracks", 100, -5, 5);
+		hZLocal = new TH1D( "ZLocal", "XLocal of Various Tracks; ZLocal; number of tracks", 100, -5, 5);
+		hChargePt = new TH1D( "ChargePt", " Charge times Pt of Various tracks; Charge times Pt; number of tracks", 100, -10, 10 );
+		hPMom = new TH1D( "PMom", "Magnitude of Momentum of Various tracks; Magnitude of Momentum; number of tracks", 100, -10, 10 );
+		hChargePMom = new TH1D( "ChargePMom", "Charge times Momentum of Various tracks; Magnitude of Momentum; number of trakcs", 100, -10, 10 );
 
 		hInvBetavsMom = new TH2D( "InvBetavsMom", "1/Beta vs Momentum; 1/Beta; Total Momentum", 1000, .8, 2, 1000, 0, 10);
 
@@ -99,9 +99,9 @@ protected:
 				btofPidTraits = _rBTofPid.get( track->bTofPidTraitsIndex() );
 			};
 
-			hDedx->Fill( track->dEdx() );
-			hphi->Fill( track->pMom().phi() );
-			hRunN->Fill( rmf.indexForRun( event->runId() ) );
+			// hDedx->Fill( track->dEdx() );
+			// hphi->Fill( track->pMom().phi() );
+			// hRunN->Fill( rmf.indexForRun( event->runId() ) );
 
 
 				if ( nullptr != btofPidTraits ){
