f02a978216eed74cf693f3661e256455c1cb9655
Seokin4862 <sy34@rice.edu>
2017-06-07 09:27:08 -0500

    pt

--- modules/PicoDstSkimmer/PicoDstSkimmer.h
+++ modules/PicoDstSkimmer/PicoDstSkimmer.h
@@ -104,6 +104,9 @@ protected:
 
 
 				if ( nullptr != btofPidTraits ){
+					hChargePt->Fill( (track->charge())*(track->gPt()) );
+					hPMom->Fill( track->pMom().mag() );
+					hChargeMom->Fill( (track->charge())*(track->pMom().mag()) );
 					hInvBetavsMom->Fill( 1.0/btofPidTraits->btofBeta(), track->pMom().mag() );
 					hMatchFlag->Fill( btofPidTraits->btofMatchFlag() );
 					hBeta->Fill( btofPidTraits->btofBeta() );
