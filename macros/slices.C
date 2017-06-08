f4a82d5f618c40f699286bfbce9320190a20666f
Seokin4862 <sy34@rice.edu>
2017-06-08 14:07:42 -0500

    check

--- macros/slices.C
+++ macros/slices.C
@@ -11,11 +11,11 @@ TH2 *hInvBeta = (TH2*)f->Get( "InvBetavsMom" );
 
 hdEdx->Draw("colz");
 c->SetLogz();
-// c->Print( "dEdxvsMom.png" );
+c->Print( "dEdxvsMom.png" );
 
 hInvBeta->Draw("colz");
 c->SetLogz();
-// c->Print( "InvBetavsMom.png" );
+c->Print( "InvBetavsMom.png" );
 
 for( int i = 1; i < 41 ; i++ ) {
 
@@ -26,18 +26,18 @@ for( int i = 1; i < 41 ; i++ ) {
   TH1 *kid = hdEdx->ProjectionY( mona.str().c_str() , m1 , m2 );
   kid->Draw();
   c->SetLogy();
-  mona << ".png";
-  c->Print( mona.str().c_str() );
+  // mona << ".png";
+  // c->Print( mona.str().c_str() );
 
   stringstream lisa;
   lisa << "lisa" << i;
   int l1 = hInvBeta->GetXaxis()->FindBin( .05*(i-1) );
   int l2 = hInvBeta->GetXaxis()->FindBin( .05(i) );
-  TH1 *africa = hInvBeta->ProjectionY( lisa.str().c_str(), l1, l2 );
-  africa->Draw();
+  TH1 *child = hInvBeta->ProjectionY( lisa.str().c_str(), l1, l2 );
+  child->Draw();
   c->SetLogy();
-  lisa << ".png";
-  c->Print( lisa.str().c_str() );
+  // lisa << ".png";
+  // c->Print( lisa.str().c_str() );
 }
 
 }
