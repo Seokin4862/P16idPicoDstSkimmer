43dbe98c90e891977df5fd62b99230d871f78d90
Seokin4862 <sy34@rice.edu>
2017-06-08 14:31:22 -0500

    poorphi

--- macros/slices.C
+++ macros/slices.C
@@ -20,26 +20,28 @@ c->Print( "InvBetavsMom.png" );
 for( int i = 1; i < 41 ; i++ ) {
 
   stringstream mona;
-  mona << "mona" << i;
+  mona << "dEdx" << i;
   int m1 = hdEdx->GetXaxis()->FindBin( .05*(i-1) );
   int m2 = hdEdx->GetXaxis()->FindBin( .05*(i) );
   TH1 *kid = hdEdx->ProjectionY( mona.str().c_str() , m1 , m2 );
   stringstream kidtitle;
   kidtitle << "dEdx for momentum range " << .05*(i-1) << " to " << .05*(i) ;
   kid->SetTitle( kidtitle.str().c_str() );
+  kid->SetYTitle( "Number of Tracks" );
   kid->Draw();
   c->SetLogy();
   mona << ".png";
   c->Print( mona.str().c_str() );
 
   stringstream lisa;
-  lisa << "lisa" << i;
+  lisa << "InvBeta" << i;
   int l1 = hInvBeta->GetXaxis()->FindBin( .05*(i-1) );
   int l2 = hInvBeta->GetXaxis()->FindBin( .05*(i) );
   TH1 *africa = hInvBeta->ProjectionY( lisa.str().c_str(), l1, l2 );
   stringstream africatitle;
-  africatitle << "InvBeta for momentum range " << .05*(i-1) << " to " << .05(i) ;
+  africatitle << "InvBeta for momentum range " << .05*(i-1) << " to " << .05*(i) ;
   africa->SetTitle( africatitle.str().c_str() );
+  africa->SetYTitle( "Number of Tracks" );
   africa->Draw();
   c->SetLogy();
   lisa << ".png";
