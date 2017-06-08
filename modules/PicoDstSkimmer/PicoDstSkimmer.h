3000bc99afbd18aecc9e9ba810566e05dbba1a21
Seokin4862 <sy34@rice.edu>
2017-06-08 11:46:15 -0500

    2dh

--- modules/PicoDstSkimmer/PicoDstSkimmer.h
+++ modules/PicoDstSkimmer/PicoDstSkimmer.h
@@ -47,39 +47,13 @@ protected:
 	TClonesArrayReader < StPicoMtdPidTraits > _rMtdPid;
 	TClonesArrayReader < StPicoBTofPidTraits > _rBTofPid;
 
-	TH1D *hDedx = 0;
-	TH1D *hphi = 0;
-	TH1D *hRunN = 0;
-	TH1D *hMatchFlag = 0;
-	TH1D *hBeta = 0;
-	TH1D *hInvBeta = 0;
-	TH1D *hYLocal = 0;
-	TH1D *hZLocal = 0;
-	TH1D *hChargePt = 0;
-	TH1D *hPMom = 0;
-	TH1D *hChargePMom = 0;
-
+	TH2D *hDedxvsMom = 0;
 	TH2D *hInvBetavsMom = 0;
-
-
 
 	void makeHistograms(){
 
-		// hDedx = new TH1D( "dEdx", "dEdx of Various Tracks; x-axis; y-axis", 1000, 0, 30 );
-		// hphi = new TH1D( "phi", "phi of Various Tracks; x-axis; y-axis", 1000, -3.2, 3.2 );
-		// hRunN = new TH1D( "RunN", "Run Numbers", 2751, 1, 2751 );
-
-		hMatchFlag = new TH1D( "MatchFlag", "MatchFlag of Various Tracks; Match Flag Numer (0, 1, 2); number of tracks", 3, -.5, 2.5);
-		hBeta = new TH1D( "Beta", "Beta of Various Tracks; Beta; number of tracks", 1000, 0, 1.5);
-		hInvBeta = new TH1D( "InvBeta", "1/Beta of Various Tracks; 1/Beta; number of tracks", 1000, 0, 1.5);
-		hYLocal = new TH1D( "YLocal", "YLocal of Various Tracks; YLocal; number of tracks", 100, -5, 5);
-		hZLocal = new TH1D( "ZLocal", "XLocal of Various Tracks; ZLocal; number of tracks", 100, -5, 5);
-		hChargePt = new TH1D( "ChargePt", " Charge times Pt of Various tracks; Charge times Pt; number of tracks", 100, -10, 10 );
-		hPMom = new TH1D( "PMom", "Magnitude of Momentum of Various tracks; Magnitude of Momentum; number of tracks", 100, -10, 10 );
-		hChargePMom = new TH1D( "ChargePMom", "Charge times Momentum of Various tracks; Magnitude of Momentum; number of trakcs", 100, -10, 10 );
-
-		hInvBetavsMom = new TH2D( "InvBetavsMom", "1/Beta vs Momentum; Total Momentum; 1/Beta", 1000, 0, 10, 1000, .8, 2);
-
+		hDedxvsMom = new TH2D( "DedxvsMom", "dEdx vs Momentum; Total Momentum; dEdx", 300, 0, 3, 1000, 0, 60 );
+		hInvBetavsMom = new TH2D( "InvBetavsMom", "1/Beta vs Momentum; Total Momentum; 1/Beta", 300, 0, 3, 1000, .8, 2);
 
 	}
 
@@ -94,26 +68,15 @@ protected:
 		for ( size_t i = 0; i < nTracks; i++ ){
 			StPicoTrack * track = _rTrack.get( i );
 
+			hDedxvsMom = new TH2D( track->pMom().mag(), track->dEdx() );
+
 			StPicoBTofPidTraits *btofPidTraits = nullptr;
 			if ( track->bTofPidTraitsIndex() >= 0 ){
 				btofPidTraits = _rBTofPid.get( track->bTofPidTraitsIndex() );
 			};
 
-			// hDedx->Fill( track->dEdx() );
-			// hphi->Fill( track->pMom().phi() );
-			// hRunN->Fill( rmf.indexForRun( event->runId() ) );
-
-
 				if ( nullptr != btofPidTraits ){
-					hChargePt->Fill( (track->charge())*(track->gPt()) );
-					hPMom->Fill( track->pMom().mag() );
-					hChargePMom->Fill( (track->charge())*(track->pMom().mag()) );
 					hInvBetavsMom->Fill( track->pMom().mag(),  1.0/btofPidTraits->btofBeta() );
-					hMatchFlag->Fill( btofPidTraits->btofMatchFlag() );
-					hBeta->Fill( btofPidTraits->btofBeta() );
-					hInvBeta->Fill( 1.0/(btofPidTraits->btofBeta()) );
-					hYLocal->Fill( btofPidTraits->btofYLocal() );
-					hZLocal->Fill( btofPidTraits->btofZLocal() );
 				}
 
 
