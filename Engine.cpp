2d73b3600ad9ece003a239767bc75b42b5326f33
Seokin4862 <sy34@rice.edu>
2017-06-12 14:06:44 -0500

    applying the correction

--- Engine.cpp
+++ Engine.cpp
@@ -26,16 +26,16 @@ int main( int argc, char* argv[] ) {
 
 // Should get rid of the below once I have things figured.
 
-	int i = 0;
-	int j =1212;
-
-	Mapper_dEdx mp( "/home/fs26/dedx_ana/quantile_dedx.root", "/home/fs26/dedx_ana/quantile_nsigmae.root" );
-
-	float corr_dEdx = 0;
-	float corr_nSigmaE = 0;
-	mp.apply_map_dEdx_nsigmaE( 0.0, 0.5, 1, 0.5, 3.2, 2, corr_dEdx, corr_nSigmaE );
-
-	LOG_F( INFO, "corr_dEdx = %f, corr_nSigmaE = %f", corr_dEdx, corr_nSigmaE );
+	// int i = 0;
+	// int j =1212;
+	// 
+	// Mapper_dEdx mp( "/home/fs26/dedx_ana/quantile_dedx.root", "/home/fs26/dedx_ana/quantile_nsigmae.root" );
+	//
+	// float corr_dEdx = 0;
+	// float corr_nSigmaE = 0;
+	// mp.apply_map_dEdx_nsigmaE( 0.0, 0.5, 1, 0.5, 3.2, 2, corr_dEdx, corr_nSigmaE );
+	//
+	// LOG_F( INFO, "corr_dEdx = %f, corr_nSigmaE = %f", corr_dEdx, corr_nSigmaE );
 
 // Stop getting rid of here.
 
