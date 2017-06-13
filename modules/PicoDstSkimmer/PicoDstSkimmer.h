bb6f1d3817def853a3ef072534800074829aeeb8
Seokin4862 <sy34@rice.edu>
2017-06-13 13:41:08 -0500

    get more music in ur repetoire

--- modules/PicoDstSkimmer/PicoDstSkimmer.h
+++ modules/PicoDstSkimmer/PicoDstSkimmer.h
@@ -92,6 +92,8 @@ protected:
 		for ( size_t i = 0; i < nTracks; i++ ){
 			StPicoTrack * track = _rTrack.get( i );
 
+			if( track->pMom().mag() < .3 ) continue;
+			if( track->pMom().mag() > .4 ) continue;
 
 			float corr_dEdx = 0;
 			float corr_nSigmaE = 0;
