74813b162d7898b1efc424f729dcffa0af4110dd
Seokin4862 <sy34@rice.edu>
2017-06-16 10:22:34 -0500

    yesterday was hell

--- macros/slices.C
+++ macros/slices.C
@@ -2,50 +2,87 @@
 
 void slices() {
 
-TFile * f = new TFile( "/home/sy34/workspace/safe/8InvBetadEdxMom/dEdxandInvBetavsMom.root" );
+TFile * f = new TFile( "/home/sy34/workspace/safe/newbtofs/newbtofs.root" );
 
 TCanvas * c = new TCanvas("c","c");
 
-TH2 *hdEdx = (TH2*)f->Get( "DedxvsMom" );
-TH2 *hInvBeta = (TH2*)f->Get( "InvBetavsMom" );
+TH2 *hdEdx = (TH2*)f->Get( "DedxvsMom1" );
+TH2 *hInvBeta = (TH2*)f->Get( "InvBetavsMom1" );
 
 hdEdx->Draw("colz");
 c->SetLogz();
-c->Print( "dEdxvsMom.png" );
+c->Print( "dEdxvsMom1.png" );
+
+for( int i = 1; i < 36 ; i++ ) {
+  stringstream angie;
+  angie << "dEdx" << i;
+  int a1 = hdEdx->GetXaxis()->FindBin( .01*(i-1) );
+  int a2 = hdEdx->GetXaxis()->FindBin( .01*(i) );
+  TH1 *bobby = hdEdx->ProjectionY( angie.str().c_str() , a1 , a2 );
+  stringstream bobbytitle;
+  bobbytitle << "dEdx for momentum range " << .01*(i-1) << " to " << .01*(i) ;
+  bobby->SetTitle( bobbytitle.str().c_str() );
+  bobby->SetYTitle( "Number of Tracks" );
+  bobby->Draw();
+  c->SetLogy();
+  angie << ".png";
+  c->Print( angie.str().c_str() );
+
+}
+
+for( int i = 7; i < 41 ; i++ ) {
+  stringstream mitchell;
+  mitchell << "dEdx" << i;
+  int c1 = hdEdx->GetXaxis()->FindBin( .05*(i-1) );
+  int c2 = hdEdx->GetXaxis()->FindBin( .05*(i) );
+  TH1 *newmark = hdEdx->ProjectionY( mitchell.str().c_str() , c1 , c2 );
+  stringstream newmarktitle;
+  newmarktitle << "dEdx for momentum range " << .05*(i-1) << " to " << .05*(i) ;
+  newmark->SetTitle( newmarktitle.str().c_str() );
+  newmark->SetYTitle( "Number of Tracks" );
+  newmark->Draw();
+  c->SetLogy();
+  mitchell << ".png";
+  c->Print( mitchell.str().c_str() );
+
+}
 
 hInvBeta->Draw("colz");
 c->SetLogz();
-c->Print( "InvBetavsMom.png" );
+c->Print( "InvBetavsMom1.png" );
 
-for( int i = 1; i < 41 ; i++ ) {
-
-  stringstream mona;
-  mona << "dEdx" << i;
-  int m1 = hdEdx->GetXaxis()->FindBin( .05*(i-1) );
-  int m2 = hdEdx->GetXaxis()->FindBin( .05*(i) );
-  TH1 *kid = hdEdx->ProjectionY( mona.str().c_str() , m1 , m2 );
-  stringstream kidtitle;
-  kidtitle << "dEdx for momentum range " << .05*(i-1) << " to " << .05*(i) ;
-  kid->SetTitle( kidtitle.str().c_str() );
-  kid->SetYTitle( "Number of Tracks" );
-  kid->Draw();
+for( int i = 1; i < 36 ; i++ ) {
+  stringstream poor;
+  poor << "InvBeta" << i;
+  int p1 = hInvBeta->GetXaxis()->FindBin( .01*(i-1) );
+  int p2 = hInvBeta->GetXaxis()->FindBin( .01*(i) );
+  TH1 *robin = hInvBeta->ProjectionY ( poor.str().c_str(), c1, c2 );
+  stringstream robintitle;
+  robintitle << "InvBeta for momentum range " << .01*(i-1) << " to " << .01*(i) ;
+  robin->SetTitle( robintitle.str().c_str() );
+  robin->SetYTitle( "Number of Tracks" );
+  robin->Draw();
   c->SetLogy();
-  mona << ".png";
-  c->Print( mona.str().c_str() );
+  poor << ".png";
+  c->Print( poor.str().c_str() );
 
-  stringstream lisa;
-  lisa << "InvBeta" << i;
-  int l1 = hInvBeta->GetXaxis()->FindBin( .05*(i-1) );
-  int l2 = hInvBeta->GetXaxis()->FindBin( .05*(i) );
-  TH1 *africa = hInvBeta->ProjectionY( lisa.str().c_str(), l1, l2 );
-  stringstream africatitle;
-  africatitle << "InvBeta for momentum range " << .05*(i-1) << " to " << .05*(i) ;
-  africa->SetTitle( africatitle.str().c_str() );
-  africa->SetYTitle( "Number of Tracks" );
-  africa->Draw();
+}
+
+for( int i = 7; i < 41; i++ ) {
+  stringstream phi;
+  phi << "InvBeta" << i;
+  int h1 = hInvBeta->GetXaxis()->FindBin( .05*(i-1) );
+  int h2 = hInvBeta->GetXaxis()->FindBin( .05*(i) );
+  TH1 *will = hInvBeta->ProjectionY( poor.str().c_str(), c1, c2 );
+  stringstream willtitle;
+  willtitle << "InvBeta for momentum range " << .01*(i-1) << " to " << .01*(i) ;
+  will->SetTitle( willtitle.str().c_str() );
+  will->SetYTitle( "Number of Tracks" );
+  will->Draw();
   c->SetLogy();
-  lisa << ".png";
-  c->Print( lisa.str().c_str() );
+  poor << ".png";
+  c->Print( poor.str().c_str() );
+
 }
 
 }
