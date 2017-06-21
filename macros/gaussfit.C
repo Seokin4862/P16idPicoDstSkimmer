111eb5c18a16cf6c06d298fea01c8027b3919c45
Seokin4862 <sy34@rice.edu>
2017-06-21 14:02:54 -0500

    I need a shower

--- macros/gaussfit.C
+++ macros/gaussfit.C
@@ -24,4 +24,21 @@ getslice << "fitted.png";
 c->Print( getslice.str().c_str() );
 }
 
+
+double bag = 0;
+double end = 20;
+
+for( int i = 8; i < 41; i++ ){
+stringstream getslice;
+getslice << "alndEdxL" << i;
+TH1 *turandot = (TH1*)f->Get( getslice.str().c_str() );
+turandot->Draw();
+c->SetLogy();
+turandot->Fit( fit, "R" , "" , bag, end );
+bag = fit->GetParameter(1) - 2*fit->GetParameter(2);
+end = fit->GetParameter(1) + 2*fit->GetParameter(2);
+getslice << "fitted.png";
+c->Print( getslice.str().c_str() );
+}
+
 }
