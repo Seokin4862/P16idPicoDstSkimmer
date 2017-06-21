7636b90ff42a3a69c30341efe036cfe56027b6f5
Seokin4862 <sy34@rice.edu>
2017-06-21 13:11:53 -0500

    fix

--- macros/gaussfit.C
+++ macros/gaussfit.C
@@ -2,17 +2,17 @@
 
 void gaussfit() {
 
-TFile *f = new TFile( "/home/sy34/workspace/safe/slices/slicesgraphs.root" );
+TFile * f = new TFile( "/home/sy34/workspace/safe/slices/slicesgraphs.root" );
 
 TCanvas *c = new TCanvas( "c", "c" );
 
-TF1 *f = new TF1( "fit", "gaus" );
+TF1 * fit = new TF1( "fit", "gaus" );
 
 for( int i = 11; i < 36; i++ ){
 stringstream getslice;
 getslice << aLndEdxK << i;
 TH1 *turandot = (TH1*)f->Get( getslice.str().c_str() );
-turandot->Fit( f );
+turandot->Fit( fit );
 }
 
 }
