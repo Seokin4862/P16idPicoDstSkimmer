129fb1b061e9edf6854b9a384275e47d10598c38
Seokin4862 <sy34@rice.edu>
2017-06-21 14:23:05 -0500

    I really need a shower

--- macros/gaussfit.C
+++ macros/gaussfit.C
@@ -41,4 +41,38 @@ getslice << "fitted.png";
 c->Print( getslice.str().c_str() );
 }
 
+
+double bag = 0;
+double end = 20;
+
+for( int i = 11; i < 36; i++ ){
+stringstream getslice;
+getslice << "aInvBetaK" << i;
+TH1 *turandot = (TH1*)f->Get( getslice.str().c_str() );
+turandot->Draw();
+c->SetLogy();
+turandot->Fit( fit, "R" , "" , bag, end );
+bag = fit->GetParameter(1) - 2*fit->GetParameter(2);
+end = fit->GetParameter(1) + 2*fit->GetParameter(2);
+getslice << "fitted.png";
+c->Print( getslice.str().c_str() );
+}
+
+
+double bag = 0;
+double end = 20;
+
+for( int i = 8; i < 41; i++ ){
+stringstream getslice;
+getslice << "aInvBetaL" << i;
+TH1 *turandot = (TH1*)f->Get( getslice.str().c_str() );
+turandot->Draw();
+c->SetLogy();
+turandot->Fit( fit, "R" , "" , bag, end );
+bag = fit->GetParameter(1) - 2*fit->GetParameter(2);
+end = fit->GetParameter(1) + 2*fit->GetParameter(2);
+getslice << "fitted.png";
+c->Print( getslice.str().c_str() );
+}
+
 }
