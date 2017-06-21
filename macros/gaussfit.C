ffb82613402298c53ae9518cbe0a01f338dd0eec
Seokin4862 <sy34@rice.edu>
2017-06-21 13:44:12 -0500

    work pls

--- macros/gaussfit.C
+++ macros/gaussfit.C
@@ -8,12 +8,17 @@ TCanvas *c = new TCanvas( "c", "c" );
 
 TF1 * fit = new TF1( "fit", "gaus" );
 
+double bag = 1.8;
+double end = 2.5;
+
 for( int i = 11; i < 36; i++ ){
 stringstream getslice;
 getslice << "alndEdxK" << i;
 TH1 *turandot = (TH1*)f->Get( getslice.str().c_str() );
 turandot->Draw();
-turandot->Fit( fit );
+turandot->Fit( fit, "R" , "" , bag, end );
+bag = fit->GetParameter(1) - fit->GetParameter(2);
+end = fit->GetParameter(1) + fit->GetParameter(2);
 }
 
 }
