6be3f7189aa49e9e5dfaa1f7bc9e254cc4e62613
Seokin4862 <sy34@rice.edu>
2017-06-26 13:57:15 -0500

    fitmomgraph

---
+++ macros/fitmomgraph.C
@@ -0,0 +1,32 @@
+
+
+void gaussfit() {
+
+TFile * f = new TFile( "/home/sy34/workspace/safe/slices/slicesgraphs.root" );
+
+TCanvas *c = new TCanvas( "c", "c" );
+
+TF1 * fit = new TF1( "fit", "gaus" );
+
+double bag = 1.5;
+double end = 2;
+int edges[35] = {};
+
+for( int i = 11; i < 36; i++ ){
+stringstream getslice;
+getslice << "alndEdxK" << i;
+TH1 *turandot = (TH1*)f->Get( getslice.str().c_str() );
+turandot->Draw();
+c->SetLogy();
+turandot->Fit( fit, "R" , "" , bag, end );
+bag = fit->GetParameter(1) - 2*fit->GetParameter(2);
+end = fit->GetParameter(1) + 2*fit->GetParameter(2);
+getslice << "fitted.png";
+double inted = f->Integrate();
+edges[i-1] = hdEdx->GetXaxis()->FindBin( .01*(i-1) );
+}
+edges[35] = .35
+TH1* intedmom = new TH1D( "intedmom" , "Integral of the Fit against Momentum Range" , 35 , edges );
+intedmom->Draw();
+
+}
--- modules/PicoDstSkimmer/PicoDstSkimmer.h
+++ modules/PicoDstSkimmer/PicoDstSkimmer.h
@@ -63,31 +63,6 @@ protected:
 	TH1D *hDedx = 0;
 	TH1D *hcorrDedx = 0;
 
-	// TH2D *hDedxvsPhi1 = 0;
-	// TH2D *hDedxvsPhi2 = 0;
-	// TH2D *hDedxvsPhi3 = 0;
-	//
-	// TH2D *hDedxvsPhi4 = 0;
-	// TH2D *hDedxvsPhi5 = 0;
-	// TH2D *hDedxvsPhi6 = 0;
-	//
-	// TH2D *hDedxvsPhi7 = 0;
-	// TH2D *hDedxvsPhi8 = 0;
-	// TH2D *hDedxvsPhi9 = 0;
-	//
-	//
-	// TH2D *hcorrDedxvsPhi1 = 0;
-	// TH2D *hcorrDedxvsPhi2 = 0;
-	// TH2D *hcorrDedxvsPhi3 = 0;
-	//
-	// TH2D *hcorrDedxvsPhi4 = 0;
-	// TH2D *hcorrDedxvsPhi5 = 0;
-	// TH2D *hcorrDedxvsPhi6 = 0;
-	//
-	// TH2D *hcorrDedxvsPhi7 = 0;
-	// TH2D *hcorrDedxvsPhi8 = 0;
-	// TH2D *hcorrDedxvsPhi9 = 0;
-
 	TH2D *hLnDedxvsMom1 = 0;
 	TH2D *hInvBetavsMom1 = 0;
 
@@ -111,31 +86,6 @@ protected:
 		hLnDedxvsMom3 = new TH2D( "LnDedxvsMom3", "Ln(dEdx) vs Momentum; Total Momentum, - charge; Ln(dEdx)", 300, 0, 3, 1000, 0, 10 );
 		hInvBetavsMom3 = new TH2D( "InvBetavsMom3", "1/Beta vs Momentum; Total Momentum, - charge; 1/Beta", 300, 0, 3, 1000, .8, 2);
 
-		// hDedxvsPhi1 = new TH2D( "DedxvsPhi1", "dEdx vs Phi, total momentum .3 to .4, = charge, = #eta; #phi; dEdx", 500, -3.2, 3.2, 500, 1.5, 6 );
-		// hDedxvsPhi2 = new TH2D( "DedxvsPhi2", "dEdx vs Phi, total momentum .3 to .4, = charge, + #eta; #phi; dEdx", 500, -3.2, 3.2, 500, 1.5, 6 );
-		// hDedxvsPhi3 = new TH2D( "DedxvsPhi3", "dEdx vs Phi, total momentum .3 to .4, = charge, - #eta; #phi; dEdx", 500, -3.2, 3.2, 500, 1.5, 6 );
-		//
-		// hDedxvsPhi4 = new TH2D( "DedxvsPhi4", "dEdx vs Phi, total momentum .3 to .4, + charge, = #eta; #phi; dEdx", 500, -3.2, 3.2, 500, 1.5, 6 );
-		// hDedxvsPhi5 = new TH2D( "DedxvsPhi5", "dEdx vs Phi, total momentum .3 to .4, + charge, + #eta; #phi; dEdx", 500, -3.2, 3.2, 500, 1.5, 6 );
-		// hDedxvsPhi6 = new TH2D( "DedxvsPhi6", "dEdx vs Phi, total momentum .3 to .4, + charge, - #eta; #phi; dEdx", 500, -3.2, 3.2, 500, 1.5, 6 );
-		//
-		// hDedxvsPhi7 = new TH2D( "DedxvsPhi7", "dEdx vs Phi, total momentum .3 to .4, - charge, = #eta; #phi; dEdx", 500, -3.2, 3.2, 500, 1.5, 6 );
-		// hDedxvsPhi8 = new TH2D( "DedxvsPhi8", "dEdx vs Phi, total momentum .3 to .4, - charge, + #eta; #phi; dEdx", 500, -3.2, 3.2, 500, 1.5, 6 );
-		// hDedxvsPhi9 = new TH2D( "DedxvsPhi9", "dEdx vs Phi, total momentum .3 to .4, - charge, - #eta; #phi; dEdx", 500, -3.2, 3.2, 500, 1.5, 6 );
-		//
-		//
-		// hcorrDedxvsPhi1 = new TH2D( "corrDedxvsPhi1", "corrected dEdx vs Phi, total momentum .3 to .4, = charge, = #eta; #phi; dEdx", 500, -3.2, 3.2, 500, 1.5, 6 );
-		// hcorrDedxvsPhi2 = new TH2D( "corrDedxvsPhi2", "corrected dEdx vs Phi, total momentum .3 to .4, = charge, + #eta; #phi; dEdx", 500, -3.2, 3.2, 500, 1.5, 6 );
-		// hcorrDedxvsPhi3 = new TH2D( "corrDedxvsPhi3", "corrected dEdx vs Phi, total momentum .3 to .4, = charge, - #eta; #phi; dEdx", 500, -3.2, 3.2, 500, 1.5, 6 );
-		//
-		// hcorrDedxvsPhi4 = new TH2D( "corrDedxvsPhi4", "corrected dEdx vs Phi, total momentum .3 to .4, + charge, = #eta; #phi; dEdx", 500, -3.2, 3.2, 500, 1.5, 6 );
-		// hcorrDedxvsPhi5 = new TH2D( "corrDedxvsPhi5", "corrected dEdx vs Phi, total momentum .3 to .4, + charge, + #eta; #phi; dEdx", 500, -3.2, 3.2, 500, 1.5, 6 );
-		// hcorrDedxvsPhi6 = new TH2D( "corrDedxvsPhi6", "corrected dEdx vs Phi, total momentum .3 to .4, + charge, - #eta; #phi; dEdx", 500, -3.2, 3.2, 500, 1.5, 6 );
-		//
-		// hcorrDedxvsPhi7 = new TH2D( "corrDedxvsPhi7", "corrected dEdx vs Phi, total momentum .3 to .4, - charge, = #eta; #phi; dEdx", 500, -3.2, 3.2, 500, 1.5, 6 );
-		// hcorrDedxvsPhi8 = new TH2D( "corrDedxvsPhi8", "corrected dEdx vs Phi, total momentum .3 to .4, - charge, + #eta; #phi; dEdx", 500, -3.2, 3.2, 500, 1.5, 6 );
-		// hcorrDedxvsPhi9 = new TH2D( "corrDedxvsPhi9", "corrected dEdx vs Phi, total momentum .3 to .4, - charge, - #eta; #phi; dEdx", 500, -3.2, 3.2, 500, 1.5, 6 );
-
 	}
 
 	virtual void analyzeEvent() {
@@ -148,73 +98,6 @@ protected:
 		size_t nTracks =  _rTrack.N();
 		for ( size_t i = 0; i < nTracks; i++ ){
 			StPicoTrack * track = _rTrack.get( i );
-
-			// if( track->pMom().mag() < .3 ) continue;
-			// if( track->pMom().mag() > .4 ) continue;
-			//
-			// float corr_dEdx = 0;
-			// float corr_nSigmaE = 0;
-			// mapper.apply_map_dEdx_nsigmaE( track->pMom().phi(), track->pMom().pseudoRapidity(), track->charge(), track->gPt(), track->dEdx(), track->nSigmaElectron(), corr_dEdx, corr_nSigmaE );
-			//
-			// hDedx->Fill( track->dEdx() );
-			// hcorrDedx->Fill( corr_dEdx );
-			//
-			// hDedxvsPhi1->Fill( track->pMom().phi(), track->dEdx() );
-			// if( track->pMom().pseudoRapidity() > 0 ){
-			// 	hDedxvsPhi2->Fill( track->pMom().phi(), track->dEdx() );
-			// }
-			// if( track->pMom().pseudoRapidity() < 0 ){
-			// 	hDedxvsPhi3->Fill( track->pMom().phi(), track->dEdx() );
-			// }
-			//
-			// if( track->charge() > 0 ){
-			// 	hDedxvsPhi4->Fill( track->pMom().phi(), track->dEdx() );
-			// 	if( track->pMom().pseudoRapidity() > 0 ){
-			// 		hDedxvsPhi5->Fill( track->pMom().phi(), track->dEdx() );
-			// 	}
-			// 	if( track->pMom().pseudoRapidity() < 0 ){
-			// 		hDedxvsPhi6->Fill( track->pMom().phi(), track->dEdx() );
-			// 	}
-			// }
-			//
-			// if( track->charge() < 0 ){
-			// 	hDedxvsPhi7->Fill( track->pMom().phi(), track->dEdx() );
-			// 	if( track->pMom().pseudoRapidity() > 0 ){
-			// 		hDedxvsPhi8->Fill( track->pMom().phi(), track->dEdx() );
-			// 	}
-			// 	if( track->pMom().pseudoRapidity() < 0 ){
-			// 		hDedxvsPhi9->Fill( track->pMom().phi(), track->dEdx() );
-			// 	}
-			// }
-			//
-			//
-			// hcorrDedxvsPhi1->Fill( track->pMom().phi(), corr_dEdx );
-			// if( track->pMom().pseudoRapidity() > 0 ){
-			// 	hcorrDedxvsPhi2->Fill( track->pMom().phi(), corr_dEdx );
-			// }
-			// if( track->pMom().pseudoRapidity() < 0 ){
-			// 	hcorrDedxvsPhi3->Fill( track->pMom().phi(), corr_dEdx );
-			// }
-			//
-			// if( track->charge() > 0 ){
-			// 	hcorrDedxvsPhi4->Fill( track->pMom().phi(), corr_dEdx );
-			// 	if( track->pMom().pseudoRapidity() > 0 ){
-			// 		hcorrDedxvsPhi5->Fill( track->pMom().phi(), corr_dEdx );
-			// 	}
-			// 	if( track->pMom().pseudoRapidity() < 0 ){
-			// 		hcorrDedxvsPhi6->Fill( track->pMom().phi(), corr_dEdx );
-			// 	}
-			// }
-			//
-			// 	if( track->charge() < 0 ){
-			// 		hcorrDedxvsPhi7->Fill( track->pMom().phi(), corr_dEdx );
-			// 		if( track->pMom().pseudoRapidity() > 0 ){
-			// 			hcorrDedxvsPhi8->Fill( track->pMom().phi(), corr_dEdx );
-			// 		}
-			// 		if( track->pMom().pseudoRapidity() < 0 ){
-			// 			hcorrDedxvsPhi9->Fill( track->pMom().phi(), corr_dEdx );
-			// 		}
-			// }
 
 			hLnDedxvsMom1->Fill( track->pMom().mag(), log(track->dEdx()) );
 
