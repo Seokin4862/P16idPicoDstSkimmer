94c5d1241b8cf83b1ca6c539f4e90106ce41e086
Seokin4862 <sy34@rice.edu>
2017-06-27 14:29:33 -0500

    I wanna leave early today

--- macros/fitbettergraphs.C
+++ macros/fitbettergraphs.C
@@ -1,7 +1,8 @@
 
 void fitbettergraphs(){
 
-TFile * f = new TFile( "" )
+TFile * f = new TFile( "/home/sy34/workspace/safe/fittedgraphs" )
 
+TCanvas *c = new TCanvas( "" )
 
 }
--- macros/fitmomgraph.C
+++ macros/fitmomgraph.C
@@ -28,37 +28,43 @@ edges1[i-1] = .01*(i-1) ;
 }
 
 edges1[35] = .35;
-TH1D * intedmom = new TH1D( "intedmom" , "Integral of the Fit against Momentum Range" , NBins1 , edges1 );
+TH1D * intedmom1 = new TH1D( "intedmom1" , "Integral of the Fit against Momentum Range" , NBins1 , edges1 );
 
 for( int i = 11; i < 36; i++ ){
-intedmom->SetBinContent( i-1, inted1[i-1] );
+intedmom1->SetBinContent( i-1, inted1[i-1] );
 }
 
+intedmom1->Draw();
 
-// double bag = .7;
-// double end = 1.3;
-// const Int_t NBins2 = 49;
-// Double_t edges2[40] = {};
-// Double_t inted2[40] = {};
-//
-//
-// for( int i = 8; i < 41; i++ ){
-// stringstream getslice;
-// getslice << "alndEdxL" << i;
-// TH1 *turandot = (TH1*)f->Get( getslice.str().c_str() );
-// turandot->Draw();
-// c->SetLogy();
-// turandot->Fit( fit, "R" , "" , bag, end );
-// bag = fit->GetParameter(1) - 2*fit->GetParameter(2);
-// end = fit->GetParameter(1) + 2*fit->GetParameter(2);
-// inted2[i-1] = fit->Integral( 0, 10);
-// edges2[i-1] = .05*(i-1) ;
-// }
-//
-// edges2[40] = ;
-//
-// Double_t aedges[76] = {};
 
-intedmom->Draw();
+double bag = .7;
+double end = 1.3;
+const Int_t NBins2 = 49;
+Double_t edges2[40] = {};
+Double_t inted2[40] = {};
+
+
+for( int i = 8; i < 41; i++ ){
+stringstream getslice;
+getslice << "alndEdxL" << i;
+TH1 *turandot = (TH1*)f->Get( getslice.str().c_str() );
+turandot->Draw();
+c->SetLogy();
+turandot->Fit( fit, "R" , "" , bag, end );
+bag = fit->GetParameter(1) - 2*fit->GetParameter(2);
+end = fit->GetParameter(1) + 2*fit->GetParameter(2);
+inted2[i-1] = fit->Integral( 0, 10);
+edges2[i-1] = .05*(i-1) ;
+}
+
+edges2[40] = 2.0;
+
+TH1D * intedmom2 = new TH1D( "intedmom2" , "Integral of the Fit against Momentum Range" , NBins2 , edges2 );
+
+for( int i = 8; i < 41; i++ ){
+intedmom2->SetBinContent( i-1, inted2[i-1] );
+}
+
+intedmom2->Draw();
 
 }
