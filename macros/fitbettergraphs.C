5519821bf09df757af98a5ef9ae2a4011c94f8e8
Seokin4862 <sy34@rice.edu>
2017-06-29 14:44:27 -0500

    three plots fix final4

--- macros/fitbettergraphs.C
+++ macros/fitbettergraphs.C
@@ -10,12 +10,26 @@ TF1 *fit = new TF1( "fit" , "gaus" );
 float binEdges[] = {0.0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.2, 0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27, 0.28, 0.29, 0.3, 0.31, 0.32, 0.33, 0.34, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0, 1.05, 1.1, 1.15, 1.2, 1.25, 1.3, 1.35, 1.4, 1.45, 1.5, 1.55, 1.6, 1.65, 1.7, 1.75, 1.8, 1.85, 1.9, 1.95, 2.0};
 int nBins = 67;
 
-TH1D * hdedxpt = new TH1D( "dedxpt", "Integral of the Pion Fit Ln(dEdx) vs Total Momentum", nBins, binEdges );
-TH1D * hdedxptm = new TH1D( "dedxptm", "Mean of the Pion Fit Ln(dEdx) vs Total Momentum", nBins, binEdges );
-TH1D * hdedxpts = new TH1D( "dedxpts", "Sigma of the Pion Fit Ln(dEdx) vs Total Momentum", nBins, binEdges );
-TH1D * hinvbpt = new TH1D( "invbpt", "Integral of the Pion Fit 1/Beta vs Total Momentum", nBins, binEdges );
-TH1D * hinvbptm = new TH1D( "invbptm", "Mean of the Pion Fit 1/Beta vs Total Momentum", nBins, binEdges );
-TH1D * hinvbpts = new TH1D( "invbpts", "Sigma of the Pion Fit 1/Beta vs Total Momentum", nBins, binEdges );
+TH1D * hadedxpt = new TH1D( "adedxpt", "Integral of the Pion Fit Ln(dEdx) vs Total Momentum", nBins, binEdges );
+TH1D * hadedxptm = new TH1D( "adedxptm", "Mean of the Pion Fit Ln(dEdx) vs Total Momentum", nBins, binEdges );
+TH1D * hadedxpts = new TH1D( "adedxpts", "Sigma of the Pion Fit Ln(dEdx) vs Total Momentum", nBins, binEdges );
+TH1D * hainvbpt = new TH1D( "ainvbpt", "Integral of the Pion Fit 1/Beta vs Total Momentum", nBins, binEdges );
+TH1D * hainvbptm = new TH1D( "ainvbptm", "Mean of the Pion Fit 1/Beta vs Total Momentum", nBins, binEdges );
+TH1D * hainvbpts = new TH1D( "ainvbpts", "Sigma of the Pion Fit 1/Beta vs Total Momentum", nBins, binEdges );
+
+TH1D * hpdedxpt = new TH1D( "pdedxpt", "Integral of the Pion Fit Ln(dEdx), + Charge vs Total Momentum", nBins, binEdges );
+TH1D * hpdedxptm = new TH1D( "pdedxptm", "Mean of the Pion Fit Ln(dEdx), + Charge vs Total Momentum", nBins, binEdges );
+TH1D * hpdedxpts = new TH1D( "pdedxpts", "Sigma of the Pion Fit Ln(dEdx), + Charge vs Total Momentum", nBins, binEdges );
+TH1D * hpinvbpt = new TH1D( "pinvbpt", "Integral of the Pion Fit 1/Beta, + Charge vs Total Momentum", nBins, binEdges );
+TH1D * hpinvbptm = new TH1D( "pinvbptm", "Mean of the Pion Fit 1/Beta, + Charge vs Total Momentum", nBins, binEdges );
+TH1D * hpinvbpts = new TH1D( "pinvbpts", "Sigma of the Pion Fit 1/Beta, + Charge vs Total Momentum", nBins, binEdges );
+
+TH1D * hndedxpt = new TH1D( "ndedxpt", "Integral of the Pion Fit Ln(dEdx), - Charge vs Total Momentum", nBins, binEdges );
+TH1D * hndedxptm = new TH1D( "ndedxptm", "Mean of the Pion Fit Ln(dEdx), - Charge vs Total Momentum", nBins, binEdges );
+TH1D * hndedxpts = new TH1D( "ndedxpts", "Sigma of the Pion Fit Ln(dEdx), - Charge vs Total Momentum", nBins, binEdges );
+TH1D * hninvbpt = new TH1D( "ninvbpt", "Integral of the Pion Fit 1/Beta, - Charge vs Total Momentum", nBins, binEdges );
+TH1D * hninvbptm = new TH1D( "ninvbptm", "Mean of the Pion Fit 1/Beta, - Charge vs Total Momentum", nBins, binEdges );
+TH1D * hninvbpts = new TH1D( "ninvbpts", "Sigma of the Pion Fit 1/Beta, - Charge vs Total Momentum", nBins, binEdges );
 
 
 double beg = 1.5;
@@ -33,9 +47,9 @@ end = fit->GetParameter(1) + 2*fit->GetParameter(2);
 temph->Draw();
 getslice<< ".png";
 // c->Print( getslice.str().c_str() );
-hdedxpt->SetBinContent( i, fit->Integral( 0, 10 )/.01 );
-hdedxptm->SetBinContent( i, fit->GetParameter(1) );
-hdedxpts->SetBinContent( i, fit->GetParameter(2) );
+hadedxpt->SetBinContent( i, fit->Integral( 0, 10 )/.01 );
+hadedxptm->SetBinContent( i, fit->GetParameter(1) );
+hadedxpts->SetBinContent( i, fit->GetParameter(2) );
 }
 
 double beg = .7;
@@ -53,18 +67,18 @@ end = fit->GetParameter(1) + 2*fit->GetParameter(2);
 temph->Draw();
 getslice<< ".png";
 // c->Print( getslice.str().c_str() );
-hdedxpt->SetBinContent( i+28, fit->Integral( 0, 10 )/.05 );
-hdedxptm->SetBinContent( i+28, fit->GetParameter(1) );
-hdedxpts->SetBinContent( i+28, fit->GetParameter(2) );
+hadedxpt->SetBinContent( i+28, fit->Integral( 0, 10 )/.05 );
+hadedxptm->SetBinContent( i+28, fit->GetParameter(1) );
+hadedxpts->SetBinContent( i+28, fit->GetParameter(2) );
 }
 
-hdedxpt->Draw();
+hadedxpt->Draw();
 c->Print( "alndEdx1.png" );
 
-hdedxptm->Draw();
+hadedxptm->Draw();
 c->Print( "alndEdx2.png" );
 
-hdedxpts->Draw();
+hadedxpts->Draw();
 c->Print( "alndEdx3.png" );
 
 
@@ -83,9 +97,9 @@ end = fit->GetParameter(1) + 2*fit->GetParameter(2);
 temph->Draw();
 getslice<< ".png";
 // c->Print( getslice.str().c_str() );
-hinvbpt->SetBinContent( i, fit->Integral( 1, 10 )/.01 );
-hinvbptm->SetBinContent( i, fit->GetParameter(1) );
-hinvbpts->SetBinContent( i, fit->GetParameter(2) );
+hainvbpt->SetBinContent( i, fit->Integral( 1, 10 )/.01 );
+hainvbptm->SetBinContent( i, fit->GetParameter(1) );
+hainvbpts->SetBinContent( i, fit->GetParameter(2) );
 }
 
 double beg = 1.04;
@@ -103,18 +117,18 @@ end = fit->GetParameter(1) + 2*fit->GetParameter(2);
 temph->Draw();
 getslice<< ".png";
 // c->Print( getslice.str().c_str() );
-hinvbpt->SetBinContent( i+28, fit->Integral( 1, 10 )/.05 );
-hinvbptm->SetBinContent( i+28, fit->GetParameter(1) );
-hinvbpts->SetBinContent( i+28, fit->GetParameter(2) );
+hainvbpt->SetBinContent( i+28, fit->Integral( 1, 10 )/.05 );
+hainvbptm->SetBinContent( i+28, fit->GetParameter(1) );
+hainvbpts->SetBinContent( i+28, fit->GetParameter(2) );
 }
 
-hinvbpt->Draw();
+hainvbpt->Draw();
 c->Print( "aInvBeta1.png" );
 
-hinvbptm->Draw();
+hainvbptm->Draw();
 c->Print( "aInvBeta2.png" );
 
-hinvbpts->Draw();
+hainvbpts->Draw();
 c->Print( "aInvBeta3.png" );
 
 }
