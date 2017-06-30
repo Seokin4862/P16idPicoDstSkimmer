ecdce76b4de60be5102206856a215060f258f30e
Seokin4862 <sy34@rice.edu>
2017-06-30 15:51:07 -0500

    centedslices.C fix 4

--- macros/centedslices.C
+++ macros/centedslices.C
@@ -15,7 +15,7 @@ for( int i = 11; i < 36; i++ ){
   name << "InvbptPia" << i;
   int n1 = hInvBeta->GetXaxis()->FindBin( .01*(i-1) );
   int n2 = hInvBeta->GetXaxis()->FindBin( .01*(i) );
-  TH1 *hist = hdEdx->ProjectionY( name.str().c_str() , n1 , n2 );
+  TH1 *hist = hInvBeta->ProjectionY( name.str().c_str() , n1 , n2 );
   stringstream histtitle;
   histtitle << "all charge 1/Beta for momentum range " << .01*(i-1) << " to " << .01*(i) << "Pion Centered" ;
   hist->SetTitle( histtitle.str().c_str() );
