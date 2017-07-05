76eaea8cfa722834f8c9faedfdc1d5e50eaea026
Seokin4862 <sy34@rice.edu>
2017-07-05 10:05:31 -0500

    forgot to commit

--- macros/centedslices.C
+++ macros/centedslices.C
@@ -17,7 +17,7 @@ for( int i = 11; i < 36; i++ ){
   int n2 = hInvBeta->GetXaxis()->FindBin( .01*(i) );
   TH1 *hist = hInvBeta->ProjectionY( name.str().c_str() , n1 , n2 );
   stringstream histtitle;
-  histtitle << "all charge 1/Beta for momentum range " << .01*(i-1) << " to " << .01*(i) << "Pion Centered" ;
+  histtitle << "1/Beta for Momentum Range for All Charge " << .01*(i-1) << " to " << .01*(i) << " Pion Centered" ;
   hist->SetTitle( histtitle.str().c_str() );
   hist->SetYTitle( "Number of Tracks" );
   hist->Write();
@@ -26,6 +26,24 @@ for( int i = 11; i < 36; i++ ){
   name << ".png";
   c->Print( name.str().c_str() );
 }
+
+for( int i = 8; i < 41; i++ ){
+  stringstream name;
+  name << "InvbptPip" << i+28;
+  int n1 = hInvBeta->GetXaxis()->FindBin( .05*(i-1) );
+  int n2 = hInvBeta->GetXaxis()->FindBin( .05*(i) );
+  TH1 *hist = hInvBeta->ProjectionY( name.str().c_str() , n1 , n2 );
+  stringstream histtitle;
+  histtitle << "1/Beta for Momentum Range for All Charge " << .05*(i-1) << " to " << .05*(i) << " Pion Centered" ;
+  hist->SetTitle( histtitle.str().c_str() );
+  hist->SetYTitle( "Number of Tracks" );
+  hist->Write();
+  hist->Draw();
+  c->SetLogy();
+  name << ".png";
+  c->Print( name.str().c_str() );
+}
+
 
 TH2 *hInvBeta = (TH2*)f->Get( "InvBetavsMomPion_p" );
 
@@ -36,7 +54,7 @@ for( int i = 11; i < 36; i++ ){
   int n2 = hInvBeta->GetXaxis()->FindBin( .01*(i) );
   TH1 *hist = hInvBeta->ProjectionY( name.str().c_str() , n1 , n2 );
   stringstream histtitle;
-  histtitle << "positive charge 1/Beta for momentum range " << .01*(i-1) << " to " << .01*(i) << "Pion Centered" ;
+  histtitle << "1/Beta for Momentum Range for Positive Charge " << .01*(i-1) << " to " << .01*(i) << " Pion Centered" ;
   hist->SetTitle( histtitle.str().c_str() );
   hist->SetYTitle( "Number of Tracks" );
   hist->Write();
@@ -45,6 +63,24 @@ for( int i = 11; i < 36; i++ ){
   name << ".png";
   c->Print( name.str().c_str() );
 }
+
+for( int i = 8; i < 41; i++ ){
+  stringstream name;
+  name << "InvbptPip" << i+28;
+  int n1 = hInvBeta->GetXaxis()->FindBin( .05*(i-1) );
+  int n2 = hInvBeta->GetXaxis()->FindBin( .05*(i) );
+  TH1 *hist = hInvBeta->ProjectionY( name.str().c_str() , n1 , n2 );
+  stringstream histtitle;
+  histtitle << "1/Beta for Momentum Range for Positive Charge " << .05*(i-1) << " to " << .05*(i) << " Pion Centered" ;
+  hist->SetTitle( histtitle.str().c_str() );
+  hist->SetYTitle( "Number of Tracks" );
+  hist->Write();
+  hist->Draw();
+  c->SetLogy();
+  name << ".png";
+  c->Print( name.str().c_str() );
+}
+
 
 TH2 *hInvBeta = (TH2*)f->Get( "InvBetavsMomPion_n" );
 
@@ -55,7 +91,24 @@ for( int i = 11; i < 36; i++ ){
   int n2 = hInvBeta->GetXaxis()->FindBin( .01*(i) );
   TH1 *hist = hInvBeta->ProjectionY( name.str().c_str() , n1 , n2 );
   stringstream histtitle;
-  histtitle << "negative charge 1/Beta for momentum range " << .01*(i-1) << " to " << .01*(i) << "Pion Centered" ;
+  histtitle << "1/Beta for Momentum Range for Negative Charge " << .01*(i-1) << " to " << .01*(i) << " Pion Centered" ;
+  hist->SetTitle( histtitle.str().c_str() );
+  hist->SetYTitle( "Number of Tracks" );
+  hist->Write();
+  hist->Draw();
+  c->SetLogy();
+  name << ".png";
+  c->Print( name.str().c_str() );
+}
+
+for( int i = 8; i < 41; i++ ){
+  stringstream name;
+  name << "InvbptPip" << i+28;
+  int n1 = hInvBeta->GetXaxis()->FindBin( .05*(i-1) );
+  int n2 = hInvBeta->GetXaxis()->FindBin( .05*(i) );
+  TH1 *hist = hInvBeta->ProjectionY( name.str().c_str() , n1 , n2 );
+  stringstream histtitle;
+  histtitle << "1/Beta for Momentum Range for Negative Charge " << .05*(i-1) << " to " << .05*(i) << " Pion Centered" ;
   hist->SetTitle( histtitle.str().c_str() );
   hist->SetYTitle( "Number of Tracks" );
   hist->Write();
