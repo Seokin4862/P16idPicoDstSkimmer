73b8047c1ccec818160750bbdf40a5e5e3ccafd9
Seokin4862 <sy34@rice.edu>
2017-07-05 10:22:37 -0500

    pions kaons and protons centered

--- macros/centedslices.C
+++ macros/centedslices.C
@@ -118,4 +118,229 @@ for( int i = 8; i < 41; i++ ){
   c->Print( name.str().c_str() );
 }
 
+
+
+TH2 *hInvBeta = (TH2*)f->Get( "InvBetavsMomKaon_a" );
+
+for( int i = 11; i < 36; i++ ){
+  stringstream name;
+  name << "InvbptKia" << i;
+  int n1 = hInvBeta->GetXaxis()->FindBin( .01*(i-1) );
+  int n2 = hInvBeta->GetXaxis()->FindBin( .01*(i) );
+  TH1 *hist = hInvBeta->ProjectionY( name.str().c_str() , n1 , n2 );
+  stringstream histtitle;
+  histtitle << "1/Beta for Momentum Range for All Charge " << .01*(i-1) << " to " << .01*(i) << " Kaon Centered" ;
+  hist->SetTitle( histtitle.str().c_str() );
+  hist->SetYTitle( "Number of Tracks" );
+  hist->Write();
+  hist->Draw();
+  c->SetLogy();
+  name << ".png";
+  c->Print( name.str().c_str() );
+}
+
+for( int i = 8; i < 41; i++ ){
+  stringstream name;
+  name << "InvbptKia" << i+28;
+  int n1 = hInvBeta->GetXaxis()->FindBin( .05*(i-1) );
+  int n2 = hInvBeta->GetXaxis()->FindBin( .05*(i) );
+  TH1 *hist = hInvBeta->ProjectionY( name.str().c_str() , n1 , n2 );
+  stringstream histtitle;
+  histtitle << "1/Beta for Momentum Range for All Charge " << .05*(i-1) << " to " << .05*(i) << " Kaon Centered" ;
+  hist->SetTitle( histtitle.str().c_str() );
+  hist->SetYTitle( "Number of Tracks" );
+  hist->Write();
+  hist->Draw();
+  c->SetLogy();
+  name << ".png";
+  c->Print( name.str().c_str() );
+}
+
+
+TH2 *hInvBeta = (TH2*)f->Get( "InvBetavsMomKaon_p" );
+
+for( int i = 11; i < 36; i++ ){
+  stringstream name;
+  name << "InvbptKip" << i;
+  int n1 = hInvBeta->GetXaxis()->FindBin( .01*(i-1) );
+  int n2 = hInvBeta->GetXaxis()->FindBin( .01*(i) );
+  TH1 *hist = hInvBeta->ProjectionY( name.str().c_str() , n1 , n2 );
+  stringstream histtitle;
+  histtitle << "1/Beta for Momentum Range for Positive Charge " << .01*(i-1) << " to " << .01*(i) << " Kaon Centered" ;
+  hist->SetTitle( histtitle.str().c_str() );
+  hist->SetYTitle( "Number of Tracks" );
+  hist->Write();
+  hist->Draw();
+  c->SetLogy();
+  name << ".png";
+  c->Print( name.str().c_str() );
+}
+
+for( int i = 8; i < 41; i++ ){
+  stringstream name;
+  name << "InvbptKip" << i+28;
+  int n1 = hInvBeta->GetXaxis()->FindBin( .05*(i-1) );
+  int n2 = hInvBeta->GetXaxis()->FindBin( .05*(i) );
+  TH1 *hist = hInvBeta->ProjectionY( name.str().c_str() , n1 , n2 );
+  stringstream histtitle;
+  histtitle << "1/Beta for Momentum Range for Positive Charge " << .05*(i-1) << " to " << .05*(i) << " Kaon Centered" ;
+  hist->SetTitle( histtitle.str().c_str() );
+  hist->SetYTitle( "Number of Tracks" );
+  hist->Write();
+  hist->Draw();
+  c->SetLogy();
+  name << ".png";
+  c->Print( name.str().c_str() );
+}
+
+TH2 *hInvBeta = (TH2*)f->Get( "InvBetavsMomKaon_n" );
+
+for( int i = 11; i < 36; i++ ){
+  stringstream name;
+  name << "InvbptKin" << i;
+  int n1 = hInvBeta->GetXaxis()->FindBin( .01*(i-1) );
+  int n2 = hInvBeta->GetXaxis()->FindBin( .01*(i) );
+  TH1 *hist = hInvBeta->ProjectionY( name.str().c_str() , n1 , n2 );
+  stringstream histtitle;
+  histtitle << "1/Beta for Momentum Range for Negative Charge " << .01*(i-1) << " to " << .01*(i) << " Kaon Centered" ;
+  hist->SetTitle( histtitle.str().c_str() );
+  hist->SetYTitle( "Number of Tracks" );
+  hist->Write();
+  hist->Draw();
+  c->SetLogy();
+  name << ".png";
+  c->Print( name.str().c_str() );
+}
+
+for( int i = 8; i < 41; i++ ){
+  stringstream name;
+  name << "InvbptKin" << i+28;
+  int n1 = hInvBeta->GetXaxis()->FindBin( .05*(i-1) );
+  int n2 = hInvBeta->GetXaxis()->FindBin( .05*(i) );
+  TH1 *hist = hInvBeta->ProjectionY( name.str().c_str() , n1 , n2 );
+  stringstream histtitle;
+  histtitle << "1/Beta for Momentum Range for Negative Charge " << .05*(i-1) << " to " << .05*(i) << " Kaon Centered" ;
+  hist->SetTitle( histtitle.str().c_str() );
+  hist->SetYTitle( "Number of Tracks" );
+  hist->Write();
+  hist->Draw();
+  c->SetLogy();
+  name << ".png";
+  c->Print( name.str().c_str() );
+}
+
+
+
+
+
+TH2 *hInvBeta = (TH2*)f->Get( "InvBetavsMomProton_a" );
+
+for( int i = 11; i < 36; i++ ){
+  stringstream name;
+  name << "InvbptPria" << i;
+  int n1 = hInvBeta->GetXaxis()->FindBin( .01*(i-1) );
+  int n2 = hInvBeta->GetXaxis()->FindBin( .01*(i) );
+  TH1 *hist = hInvBeta->ProjectionY( name.str().c_str() , n1 , n2 );
+  stringstream histtitle;
+  histtitle << "1/Beta for Momentum Range for All Charge " << .01*(i-1) << " to " << .01*(i) << " Proton Centered" ;
+  hist->SetTitle( histtitle.str().c_str() );
+  hist->SetYTitle( "Number of Tracks" );
+  hist->Write();
+  hist->Draw();
+  c->SetLogy();
+  name << ".png";
+  c->Print( name.str().c_str() );
+}
+
+for( int i = 8; i < 41; i++ ){
+  stringstream name;
+  name << "InvbptPria" << i+28;
+  int n1 = hInvBeta->GetXaxis()->FindBin( .05*(i-1) );
+  int n2 = hInvBeta->GetXaxis()->FindBin( .05*(i) );
+  TH1 *hist = hInvBeta->ProjectionY( name.str().c_str() , n1 , n2 );
+  stringstream histtitle;
+  histtitle << "1/Beta for Momentum Range for All Charge " << .05*(i-1) << " to " << .05*(i) << " Proton Centered" ;
+  hist->SetTitle( histtitle.str().c_str() );
+  hist->SetYTitle( "Number of Tracks" );
+  hist->Write();
+  hist->Draw();
+  c->SetLogy();
+  name << ".png";
+  c->Print( name.str().c_str() );
+}
+
+
+TH2 *hInvBeta = (TH2*)f->Get( "InvBetavsMomProton_p" );
+
+for( int i = 11; i < 36; i++ ){
+  stringstream name;
+  name << "InvbptPrip" << i;
+  int n1 = hInvBeta->GetXaxis()->FindBin( .01*(i-1) );
+  int n2 = hInvBeta->GetXaxis()->FindBin( .01*(i) );
+  TH1 *hist = hInvBeta->ProjectionY( name.str().c_str() , n1 , n2 );
+  stringstream histtitle;
+  histtitle << "1/Beta for Momentum Range for Positive Charge " << .01*(i-1) << " to " << .01*(i) << " Proton Centered" ;
+  hist->SetTitle( histtitle.str().c_str() );
+  hist->SetYTitle( "Number of Tracks" );
+  hist->Write();
+  hist->Draw();
+  c->SetLogy();
+  name << ".png";
+  c->Print( name.str().c_str() );
+}
+
+for( int i = 8; i < 41; i++ ){
+  stringstream name;
+  name << "InvbptPrip" << i+28;
+  int n1 = hInvBeta->GetXaxis()->FindBin( .05*(i-1) );
+  int n2 = hInvBeta->GetXaxis()->FindBin( .05*(i) );
+  TH1 *hist = hInvBeta->ProjectionY( name.str().c_str() , n1 , n2 );
+  stringstream histtitle;
+  histtitle << "1/Beta for Momentum Range for Positive Charge " << .05*(i-1) << " to " << .05*(i) << " Proton Centered" ;
+  hist->SetTitle( histtitle.str().c_str() );
+  hist->SetYTitle( "Number of Tracks" );
+  hist->Write();
+  hist->Draw();
+  c->SetLogy();
+  name << ".png";
+  c->Print( name.str().c_str() );
+}
+
+
+TH2 *hInvBeta = (TH2*)f->Get( "InvBetavsMomProton_n" );
+
+for( int i = 11; i < 36; i++ ){
+  stringstream name;
+  name << "InvbptPrin" << i;
+  int n1 = hInvBeta->GetXaxis()->FindBin( .01*(i-1) );
+  int n2 = hInvBeta->GetXaxis()->FindBin( .01*(i) );
+  TH1 *hist = hInvBeta->ProjectionY( name.str().c_str() , n1 , n2 );
+  stringstream histtitle;
+  histtitle << "1/Beta for Momentum Range for Negative Charge " << .01*(i-1) << " to " << .01*(i) << " Proton Centered" ;
+  hist->SetTitle( histtitle.str().c_str() );
+  hist->SetYTitle( "Number of Tracks" );
+  hist->Write();
+  hist->Draw();
+  c->SetLogy();
+  name << ".png";
+  c->Print( name.str().c_str() );
+}
+
+for( int i = 8; i < 41; i++ ){
+  stringstream name;
+  name << "InvbptPrin" << i+28;
+  int n1 = hInvBeta->GetXaxis()->FindBin( .05*(i-1) );
+  int n2 = hInvBeta->GetXaxis()->FindBin( .05*(i) );
+  TH1 *hist = hInvBeta->ProjectionY( name.str().c_str() , n1 , n2 );
+  stringstream histtitle;
+  histtitle << "1/Beta for Momentum Range for Negative Charge " << .05*(i-1) << " to " << .05*(i) << " Proton Centered" ;
+  hist->SetTitle( histtitle.str().c_str() );
+  hist->SetYTitle( "Number of Tracks" );
+  hist->Write();
+  hist->Draw();
+  c->SetLogy();
+  name << ".png";
+  c->Print( name.str().c_str() );
+}
+
 }
