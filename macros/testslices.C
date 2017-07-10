9823412c300e6dbde41231a5e1f68be66e939d6c
Seokin4862 <sy34@rice.edu>
2017-07-10 13:17:39 -0500

    testslicesfix2

--- macros/testslices.C
+++ macros/testslices.C
@@ -10,7 +10,7 @@ void testslices(){
 
   TH2 *hInvBeta = (TH2*)f->Get( "InvBetavsMomPion_a" );
 
-  for( int i = 11; i < 200; i++ ){
+  for( int i = 16; i < 200; i++ ){
     stringstream name;
     name << "InvbptPia" << i;
     int n1 = hInvBeta->GetXaxis()->FindBin( .01*(i-1) );
@@ -25,7 +25,7 @@ void testslices(){
 
   TH2 *hInvBeta = (TH2*)f->Get( "InvBetavsMomPion_p" );
 
-  for( int i = 11; i < 200; i++ ){
+  for( int i = 16; i < 200; i++ ){
     stringstream name;
     name << "InvbptPip" << i;
     int n1 = hInvBeta->GetXaxis()->FindBin( .01*(i-1) );
@@ -41,7 +41,7 @@ void testslices(){
 
   TH2 *hInvBeta = (TH2*)f->Get( "InvBetavsMomPion_n" );
 
-  for( int i = 11; i < 200; i++ ){
+  for( int i = 16; i < 200; i++ ){
     stringstream name;
     name << "InvbptPin" << i;
     int n1 = hInvBeta->GetXaxis()->FindBin( .01*(i-1) );
@@ -57,7 +57,7 @@ void testslices(){
 
   TH2 *hInvBeta = (TH2*)f->Get( "InvBetavsMomKaon_a" );
 
-  for( int i = 11; i < 200; i++ ){
+  for( int i = 16; i < 200; i++ ){
     stringstream name;
     name << "InvbptKia" << i;
     int n1 = hInvBeta->GetXaxis()->FindBin( .01*(i-1) );
@@ -72,7 +72,7 @@ void testslices(){
 
   TH2 *hInvBeta = (TH2*)f->Get( "InvBetavsMomKaon_p" );
 
-  for( int i = 11; i < 200; i++ ){
+  for( int i = 16; i < 200; i++ ){
     stringstream name;
     name << "InvbptKip" << i;
     int n1 = hInvBeta->GetXaxis()->FindBin( .01*(i-1) );
@@ -87,7 +87,7 @@ void testslices(){
 
   TH2 *hInvBeta = (TH2*)f->Get( "InvBetavsMomKaon_n" );
 
-  for( int i = 11; i < 200; i++ ){
+  for( int i = 16; i < 200; i++ ){
     stringstream name;
     name << "InvbptKin" << i;
     int n1 = hInvBeta->GetXaxis()->FindBin( .01*(i-1) );
@@ -103,7 +103,7 @@ void testslices(){
 
   TH2 *hInvBeta = (TH2*)f->Get( "InvBetavsMomProton_a" );
 
-  for( int i = 11; i < 36; i++ ){
+  for( int i = 16; i < 36; i++ ){
     stringstream name;
     name << "InvbptPria" << i;
     int n1 = hInvBeta->GetXaxis()->FindBin( .01*(i-1) );
@@ -118,7 +118,7 @@ void testslices(){
 
   TH2 *hInvBeta = (TH2*)f->Get( "InvBetavsMomProton_p" );
 
-  for( int i = 11; i < 36; i++ ){
+  for( int i = 16; i < 36; i++ ){
     stringstream name;
     name << "InvbptPrip" << i;
     int n1 = hInvBeta->GetXaxis()->FindBin( .01*(i-1) );
@@ -133,7 +133,7 @@ void testslices(){
 
   TH2 *hInvBeta = (TH2*)f->Get( "InvBetavsMomProton_n" );
 
-  for( int i = 11; i < 36; i++ ){
+  for( int i = 16; i < 36; i++ ){
     stringstream name;
     name << "InvbptPrin" << i;
     int n1 = hInvBeta->GetXaxis()->FindBin( .01*(i-1) );
@@ -145,6 +145,4 @@ void testslices(){
     hist->SetYTitle( "Number of Tracks" );
     hist->Write();
   }
-
-
 }
