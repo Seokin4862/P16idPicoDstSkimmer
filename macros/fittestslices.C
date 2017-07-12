4195f06b715160e387c5c6b51e7378cdc7f4b00c
Seokin4862 <sy34@rice.edu>
2017-07-12 13:33:40 -0500

    parameters fixed2?

--- macros/fittestslices.C
+++ macros/fittestslices.C
@@ -95,8 +95,8 @@ void fittestslices(){
 
 
 	TF1 *eval = new TF1( "eval", PearsonVII, -1, 1, 4 );
-	eval->SetParameters(0, .1, 1000, 0);
-	eval->SetParNames( "mean", "sigma", "yield", "lambda" );
+	// eval->SetParameters(0, .1, 1000, 0);
+	// eval->SetParNames( "mean", "sigma", "yield", "lambda" );
 
   double beg = -.05;
   double end = .05;
