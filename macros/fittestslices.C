26aecd76ae62f54717f8a30cd9de8f3419ec0262
Seokin4862 <sy34@rice.edu>
2017-07-14 11:49:13 -0500

    L

--- macros/fittestslices.C
+++ macros/fittestslices.C
@@ -59,48 +59,50 @@ void fittestslices(){
 
   TH1D * hinvbpia = new TH1D( "hinvbpia", "Integral of the Pion Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
 	TH1D * hpearsonpia = new TH1D( "hpearsonpia", "Integral of the Pion Fit (PearsonVII) vs Total Momentum", 200, 0, 2 );
+	TH1D * hinvbpiaL = new TH1D( "hinvbpiaL", "Integral of the Pion Fit (Gaussian Log Likelihood) vs Total Momentum", 200, 0, 2 );
+	TH1D * hpearsonpiaL = new TH1D( "hpearsonpiaL", "Integral of the Pion Fit (PearsonVII Log Likelihood) vs Total Momentum", 200, 0, 2 );
   TH1D * hinvbpiam = new TH1D( "hinvbpiam", "Mean of the Pion Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
   TH1D * hinvbpias = new TH1D( "hinvbpias", "Sigma of the Pion Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
 
-  TH1D * hinvbpip = new TH1D( "hinvbpip", "Integral of the Pion Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
-	TH1D * hpearsonpip = new TH1D( "hpearsonpip", "Integral of the Pion Fit (PearsonVII) vs Total Momentum", 200, 0, 2 );
-  TH1D * hinvbpipm = new TH1D( "hinvbpipm", "Mean of the Pion Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
-  TH1D * hinvbpips = new TH1D( "hinvbpips", "Sigma of the Pion Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
-
-  TH1D * hinvbpin = new TH1D( "hinvbpin", "Integral of the Pion Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
-	TH1D * hpearsonpin = new TH1D( "hpearsonpin", "Integral of the Pion Fit (PearsonVII) vs Total Momentum", 200, 0, 2 );
-  TH1D * hinvbpinm = new TH1D( "hinvbpinm", "Mean of the Pion Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
-  TH1D * hinvbpins = new TH1D( "hinvbpins", "Sigma of the Pion Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
-
-  TH1D * hinvbkia = new TH1D( "hinvbkia", "Integral of the Kaon Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
-	TH1D * hpearsonkia = new TH1D( "hpearsonkia", "Integral of the Kaon Fit (PearsonVII) vs Total Momentum", 200, 0, 2 );
-  TH1D * hinvbkiam = new TH1D( "hinvbkiam", "Mean of the Kaon Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
-  TH1D * hinvbkias = new TH1D( "hinvbkias", "Sigma of the Kaon Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
-
-  TH1D * hinvbkip = new TH1D( "hinvbkip", "Integral of the Kaon Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
-	TH1D * hpearsonkip = new TH1D( "hpearsonkip", "Integral of the Kaon Fit (PearsonVII) vs Total Momentum", 200, 0, 2 );
-  TH1D * hinvbkipm = new TH1D( "hinvbkipm", "Mean of the Kaon Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
-  TH1D * hinvbkips = new TH1D( "hinvbkips", "Sigma of the Kaon Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
-
-  TH1D * hinvbkin = new TH1D( "hinvbkin", "Integral of the Kaon Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
-	TH1D * hpearsonkin = new TH1D( "hpearsonkin", "Integral of the Kaon Fit (PearsonVII) vs Total Momentum", 200, 0, 2 );
-  TH1D * hinvbkinm = new TH1D( "hinvbkinm", "Mean of the Kaon Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
-  TH1D * hinvbkins = new TH1D( "hinvbkins", "Sigma of the Kaon Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
-
-  TH1D * hinvbpria = new TH1D( "hinvbpria", "Integral of the Proton Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
-	TH1D * hpearsonpria = new TH1D( "hpearsonpria", "Integral of the Proton Fit (PearsonVII) vs Total Momentum", 200, 0, 2 );
-  TH1D * hinvbpriam = new TH1D( "hinvbpriam", "Mean of the Proton Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
-  TH1D * hinvbprias = new TH1D( "hinvbprias", "Sigma of the Proton Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
-
-  TH1D * hinvbprip = new TH1D( "hinvbprip", "Integral of the Proton Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
-	TH1D * hpearsonprip = new TH1D( "hpearsonprip", "Integral of the Proton Fit (PearsonVII) vs Total Momentum", 200, 0, 2 );
-  TH1D * hinvbpripm = new TH1D( "hinvbpripm", "Mean of the Proton Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
-  TH1D * hinvbprips = new TH1D( "hinvbprips", "Sigma of the Proton Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
-
-  TH1D * hinvbprin = new TH1D( "hinvbprin", "Integral of the Proton Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
-	TH1D * hpearsonprin = new TH1D( "hpearsonprin", "Integral of the Proton Fit (PearsonVII) vs Total Momentum", 200, 0, 2 );
-  TH1D * hinvbprinm = new TH1D( "hinvbprinm", "Mean of the Proton Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
-  TH1D * hinvbprins = new TH1D( "hinvbprins", "Sigma of the Proton Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
+  // TH1D * hinvbpip = new TH1D( "hinvbpip", "Integral of the Pion Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
+	// TH1D * hpearsonpip = new TH1D( "hpearsonpip", "Integral of the Pion Fit (PearsonVII) vs Total Momentum", 200, 0, 2 );
+  // TH1D * hinvbpipm = new TH1D( "hinvbpipm", "Mean of the Pion Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
+  // TH1D * hinvbpips = new TH1D( "hinvbpips", "Sigma of the Pion Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
+	//
+  // TH1D * hinvbpin = new TH1D( "hinvbpin", "Integral of the Pion Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
+	// TH1D * hpearsonpin = new TH1D( "hpearsonpin", "Integral of the Pion Fit (PearsonVII) vs Total Momentum", 200, 0, 2 );
+  // TH1D * hinvbpinm = new TH1D( "hinvbpinm", "Mean of the Pion Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
+  // TH1D * hinvbpins = new TH1D( "hinvbpins", "Sigma of the Pion Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
+	//
+  // TH1D * hinvbkia = new TH1D( "hinvbkia", "Integral of the Kaon Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
+	// TH1D * hpearsonkia = new TH1D( "hpearsonkia", "Integral of the Kaon Fit (PearsonVII) vs Total Momentum", 200, 0, 2 );
+  // TH1D * hinvbkiam = new TH1D( "hinvbkiam", "Mean of the Kaon Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
+  // TH1D * hinvbkias = new TH1D( "hinvbkias", "Sigma of the Kaon Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
+	//
+  // TH1D * hinvbkip = new TH1D( "hinvbkip", "Integral of the Kaon Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
+	// TH1D * hpearsonkip = new TH1D( "hpearsonkip", "Integral of the Kaon Fit (PearsonVII) vs Total Momentum", 200, 0, 2 );
+  // TH1D * hinvbkipm = new TH1D( "hinvbkipm", "Mean of the Kaon Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
+  // TH1D * hinvbkips = new TH1D( "hinvbkips", "Sigma of the Kaon Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
+	//
+  // TH1D * hinvbkin = new TH1D( "hinvbkin", "Integral of the Kaon Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
+	// TH1D * hpearsonkin = new TH1D( "hpearsonkin", "Integral of the Kaon Fit (PearsonVII) vs Total Momentum", 200, 0, 2 );
+  // TH1D * hinvbkinm = new TH1D( "hinvbkinm", "Mean of the Kaon Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
+  // TH1D * hinvbkins = new TH1D( "hinvbkins", "Sigma of the Kaon Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
+	//
+  // TH1D * hinvbpria = new TH1D( "hinvbpria", "Integral of the Proton Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
+	// TH1D * hpearsonpria = new TH1D( "hpearsonpria", "Integral of the Proton Fit (PearsonVII) vs Total Momentum", 200, 0, 2 );
+  // TH1D * hinvbpriam = new TH1D( "hinvbpriam", "Mean of the Proton Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
+  // TH1D * hinvbprias = new TH1D( "hinvbprias", "Sigma of the Proton Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
+	//
+  // TH1D * hinvbprip = new TH1D( "hinvbprip", "Integral of the Proton Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
+	// TH1D * hpearsonprip = new TH1D( "hpearsonprip", "Integral of the Proton Fit (PearsonVII) vs Total Momentum", 200, 0, 2 );
+  // TH1D * hinvbpripm = new TH1D( "hinvbpripm", "Mean of the Proton Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
+  // TH1D * hinvbprips = new TH1D( "hinvbprips", "Sigma of the Proton Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
+	//
+  // TH1D * hinvbprin = new TH1D( "hinvbprin", "Integral of the Proton Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
+	// TH1D * hpearsonprin = new TH1D( "hpearsonprin", "Integral of the Proton Fit (PearsonVII) vs Total Momentum", 200, 0, 2 );
+  // TH1D * hinvbprinm = new TH1D( "hinvbprinm", "Mean of the Proton Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
+  // TH1D * hinvbprins = new TH1D( "hinvbprins", "Sigma of the Proton Fit (Gaussian) vs Total Momentum", 200, 0, 2 );
 
 
 	TF1 *eval = new TF1( "eval", PearsonVII, -1, 1, 4 );
@@ -130,8 +132,12 @@ void fittestslices(){
     // c->Print( getslice.str().c_str() );
     hinvbpia->SetBinContent( i, fit->Integral( -.1, .1 )/.01 );
 		hpearsonpia->SetBinContent( i, eval->Integral( -1.5, 1.5 )/.01 );
+		hinvbpiaL->SetBinContent( i, fitL->Integral( -.1, .1 )/.01 );
+		hpearsonpiaL->SetBinContent( i, evalL->Integral( -1.5, 1.5 )/.01 );
     hinvbpiam->SetBinContent( i, fit->GetParameter(1) );
     hinvbpias->SetBinContent( i, fit->GetParameter(2) );
+
+
   }
 
   hinvbpia->Write();
