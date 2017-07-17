fe3477f9983434d6d6b85f9033bac832916ed659
Seokin4862 <sy34@rice.edu>
2017-07-17 15:02:26 -0500

    ...

--- macros/thrDSlices.C
+++ macros/thrDSlices.C
@@ -7,11 +7,7 @@ void thrDSlices(){
 
   TCanvas * c = new TCanvas( "c","c" );
 
-  TH3 *hInvBetaDedxMomPion = (TH3*)f->Get( "InvBetaDedxMomPion" );
-  TH3 *hInvBetaDedxMomKaon = (TH3*)f->Get( "InvBetaDedxMomKaon" );
-  TH3 *hInvBetaDedxMomProton = (TH3*)f->Get( "InvBetaDedxMomProton" );
-
-  for( int i = 5; i  < 201; i++ ){
+  for( int i = 1; i  < 201; i++ ){
     TH3 *hInvBetaDedxMom = (TH3*)f->Get( "InvBetaDedxMom" );
     stringstream slicename;
     slicename << "hInvBetaDedx" << i;
@@ -27,7 +23,64 @@ void thrDSlices(){
     slice->Write();
     slice->Draw();
     slicename << ".png";
-    c->Print( slicename.str().c_str() );
+    // c->Print( slicename.str().c_str() );
+  }
+
+  for( int i = 1; i < 201; i++ ){
+    TH3 *hInvBetaDedxMomPion = (TH3*)f->Get( "InvBetaDedxMomPion" );
+    stringstream slicename;
+    slicename << "hInvBetaDedxPion" << i;
+    int a1 = hInvBetaDedxMomPion->GetZaxis()->FindBin( .01*(i-1) );
+    int a2 = hInvBetaDedxMomPion->GetZaxis()->FindBin( .01*(i) );
+    hInvBetaDedxMomPion->GetZaxis()->SetRange( a1, a2 );
+    TH2 *slice = (TH2*)hInvBetaDedxMomPion->Project3D( "xy" );
+    stringstream slicetitle;
+    slicetitle << "" << .01*(i-1) << " to " << .01*(i);
+    slice->SetTitle( slicetitle.str().c_str() );
+    slice->SetXTitle( "dE/dx" );
+    slice->SetYTitle( "1/Beta" );
+    slice->Write();
+    slice->Draw();
+    slicename << ".png";
+    // c->Print( slicename.str().c_str() );
+  }
+
+  for( int i = 1, i < 201; i++ ){
+    TH3 *hInvBetaDedxMomKaon = (TH3*)f->Get( "InvBetaDedxMomKaon" );
+    stringstream slicename;
+    slicename << "hInvBetaDedxKaon" << i;
+    int a1 = hInvBetaDedxMomKaon->GetZaxis()->FindBin( .01*(i-1) );
+    int a2 = hInvBetaDedxMomKaon->GetZaxis()->FindBin( .01*(i) );
+    hInvBetaDedxMomKaon->GetZaxis()->SetRange( a1, a2 );
+    TH2 *slice = (TH2*)hInvBetaDedxMomKaon->Project3D( "xy" );
+    stringstream slicetitle;
+    slicetitle << "" << .01*(i-1) << " to " << .01*(i);
+    slice->SetTitle( slicetitle.str().c_str() );
+    slice->SetXTitle( "dE/dx" );
+    slice->SetYTitle( "1/Beta" );
+    slice->Write();
+    slice->Draw();
+    slicename << ".png";
+    // c->Print( slicename.str().c_str() );
+  }
+
+  for( int i=1, i < 201; i++ ){
+    TH3 *hInvBetaDedxMomProton = (TH3*)f->Get( "InvBetaDedxMomProton" );
+    stringstream slicename;
+    slicename << "hInvBetaDedxProton" << i;
+    int a1 = hInvBetaDedxMomProton->GetZaxis()->FindBin( .01*(i-1) );
+    int a2 = hInvBetaDedxMomProton->GetZaxis()->FindBin( .01*(i) );
+    hInvBetaDedxMomProton->GetZaxis()->SetRange( a1, a2 );
+    TH2 *slice = (TH2*)hInvBetaDedxMomProton->Project3D( "xy" );
+    stringstream slicetitle;
+    slicetitle << "" << .01*(i-1) << " to " << .01*(i);
+    slice->SetTitle( slicetitle.str().c_str() );
+    slice->SetXTitle( "dE/dx" );
+    slice->SetYTitle( "1/Beta" );
+    slice->Write();
+    slice->Draw();
+    slicename << ".png";
+    // c->Print( slicename.str().c_str() );
   }
 
 }
